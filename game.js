970c4a72d02a0c924d638ed0bf141bcacbce45cd
companyforme <companyforme@gmail.com>
2020-01-26 22:13:17 +0800

    init 入口

--- game.js
+++ game.js
@@ -1,14 +1,8 @@
-//game.js
-const canvas = wx.createCanvas()
-const context = canvas.getContext('2d') // 创建一个 2d context
-context.fillStyle = '#1aad19' // 矩形颜色
-const { windowWidth, windowHeight } = wx.getSystemInfoSync()
-function drawRect(x, y) {
-  context.clearRect(0, 0, windowWidth, windowHeight)
-  context.fillRect(x, y, 100, 100)
-}
-const rectX = canvas.width / 2 - 50
-let rectY = 0
-setInterval(function () {
-  drawRect(rectX, rectY++)
-}, 16)
+/*
+ * @file: game入口
+ * @author: Chen Xiao
+ * @Date: 2020-01-26 12:08:38
+ */
+import Main from './js/main'
+
+new Main()
